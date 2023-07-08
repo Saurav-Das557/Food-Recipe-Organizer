e2a263d2a1283a25c08d04aac2702e1275fe1778
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-07-08 21:17:22 +0600

    added loginController for handling login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPass } from "../helpers/authHelper.js";
+import { comparePass, hashPass } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
   try {
@@ -51,6 +52,60 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in Registration",
+      error,
+    });
+  }
+};
+
+// POST LOGIN
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    //check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Email is not registered",
+      });
+    }
+    const match = await comparePass(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    // token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "Login Successful",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
       error,
     });
   }
