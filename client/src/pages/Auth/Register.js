61f4f445b5a8b47a496b9b6433602bfb1d071005
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-07-26 22:01:26 +0600

    created register page and added style

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone);
+  };
+
   return (
-    <Layout>
-      <h1>Register page</h1>
+    <Layout title="Register">
+      <div className="register">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputName" className="form-label">
+              Phone
+            </label>
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
