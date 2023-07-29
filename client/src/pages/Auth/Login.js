46af76122aa0a671d1e5de1a155d6bb6c4f8ecce
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-07-29 23:56:22 +0600

    added Context API

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,11 +4,12 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../styles/authStyles.css";
+import { useAuth } from "../../context/Auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [auth, SetAuth] = useAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -16,10 +17,16 @@ const Login = () => {
     try {
       const res = await axios.post(
         `${process.env.REACT_APP_API}/api/v1/auth/login`,
-        { email, password}
+        { email, password }
       );
       if (res && res.data.success) {
         toast.success(res.data && res.data.message, { duration: 2000 });
+        SetAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+        localStorage.setItem("auth", JSON.stringify(res.data));
         navigate("/");
       } else {
         toast.error(res.data.message);
@@ -34,8 +41,7 @@ const Login = () => {
     <Layout title="Login">
       <div className="form-container">
         <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-          </div>
+          <div className="mb-3"></div>
           <div className="mb-3">
             <label htmlFor="exampleInputName" className="form-label">
               Email
