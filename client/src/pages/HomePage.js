12fc03f18e5d2e1018576e80f1cd7c13474d7e51
Shammo <58135476+Arc4NE01@users.noreply.github.com>
2023-08-13 03:13:39 +0600

    added homepage component and styles

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { useAuth } from "../context/Auth";
+import Category from "../components/API/Category";
+import Search from "../components/API/Search";
+import Popular from "../components/API/Popular";
+import { motion } from "framer-motion";
+import Vegetable from "../components/API/Vegetable";
 
 const HomePage = () => {
   const [auth, setAuth] = useAuth();
   return (
     <Layout>
-      <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        exits={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Search />
+        <Category />
+        <Vegetable />
+        <Popular />
+        {/* <pre>{JSON.stringify(auth, null, 4)}</pre> */}
+      </motion.div>
     </Layout>
   );
 };
